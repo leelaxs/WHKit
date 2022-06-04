8f7b7baa3ea8835afb985795df4beea9a44755f7
Jlz <losaic@sina.cn>
2022-06-04 14:29:16 +0800

    修改button backImage 为空时控制台的错误输出

--- WHKit/UIButton+WHButton.m
+++ WHKit/UIButton+WHButton.m
@@ -31,12 +31,20 @@ NSString const *UIButton_badgeValueKey = @"UIButton_badgeValueKey";
 @dynamic badgePadding, badgeMinSize, badgeOriginX, badgeOriginY;
 @dynamic shouldHideBadgeAtZero, shouldAnimateBadge;
 
-+(instancetype)wh_buttonWithTitle:(NSString *)title backColor:(UIColor *)backColor backImageName:(NSString *)backImageName titleColor:(UIColor *)color fontSize:(int)fontSize frame:(CGRect)frame cornerRadius:(CGFloat)cornerRadius {
++(instancetype)wh_buttonWithTitle:(NSString *)title
+                        backColor:(UIColor *)backColor
+                    backImageName:(NSString *)backImageName
+                       titleColor:(UIColor *)color
+                         fontSize:(int)fontSize
+                            frame:(CGRect)frame
+                     cornerRadius:(CGFloat)cornerRadius {
     
     UIButton *button = [UIButton new];
     [button setTitle:title forState:UIControlStateNormal];
     [button setBackgroundColor:backColor];
-    [button setBackgroundImage:[UIImage imageNamed:backImageName] forState:UIControlStateNormal];
+    if (backImageName && ![@"" isEqualToString:backImageName]) {
+        [button setBackgroundImage:[UIImage imageNamed:backImageName] forState:UIControlStateNormal];
+    }
     [button setTitleColor:color forState:UIControlStateNormal];
     button.titleLabel.font = [UIFont systemFontOfSize:fontSize];
     [button sizeToFit];
